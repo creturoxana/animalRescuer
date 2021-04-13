12f3bba9c0d8a721d63fa935df8574f97dab2809
Roxana Cretu <cretu.roxanna@gmail.com>
2021-04-13 11:58:48 +0300

    changed ""=="  to equals

--- src/main/java/org/example/Adopter.java
+++ src/main/java/org/example/Adopter.java
@@ -11,7 +11,7 @@ public class Adopter extends Human {
   public void feed(Animal animal, AnimalFood animalFood) {
     System.out.println(super.name + " just gave some " + animalFood.name + " to " + animal.name);
     animal.hungerLevel--;
-    if(animalFood.name == animal.favouriteFood){
+    if(animalFood.name.equals(animal.favouriteFood)){
       //animal.temperLevel++;
       System.out.println(animal.temperLevel+=1);
     }
@@ -21,7 +21,7 @@ public class Adopter extends Human {
   public void activity(Animal animal, RecreationActivity activity) {
     System.out.println(super.name + " " + activity.name + " with " + animal.name);
     animal.temperLevel++;
-    if(activity.name == animal.favouriteActivity) {
+    if(activity.name.equals(animal.favouriteActivity)) {
       animal.temperLevel+=2;
     }
     else animal.temperLevel++;
